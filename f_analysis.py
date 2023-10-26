a52a8cbdb34dff3e9f6fb76ff0dbe802d68cad29
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-26 11:10:20 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -197,7 +197,7 @@ from f_analysis import financial_analyst
 def main():
     st.title("Stock analyzer")
 
-    company_name = st.text_input("Company name:")
+    company_name = st.text_input("Company name:", key="company_name")
     analyze_button = st.button("Analyze")
 
     if analyze_button:
