994d21cf0a8e9a9e5c876c3f36fb40a63c893c66
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-26 12:48:12 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -12,10 +12,11 @@ from dotenv import load_dotenv
 load_dotenv()
 # Set up LLM
 # Get an OpenAI API Key before continuing
+count = 0
 if "openai.api_key" in st.secrets:
     openai.api_key = st.secrets.openai_api_key
 else:
-    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password", key='api')
+    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password", key=count)
 if not openai.api_key:
     st.title("Hello, I'm your Stock Analyzer")
     st.info("Enter an OpenAI API Key to continue")
@@ -23,7 +24,7 @@ if not openai.api_key:
     st.info( " 1) Please visit https://platform.openai.com/account/api-keys")
     st.info(" 2) Click on 'Create new key' and copy and save the key in a safe location")
     st.stop()
-    
+count += 1    
 #Define Functions
 def get_company_news(company_name):
   
@@ -198,7 +199,7 @@ from f_analysis import financial_analyst
 
 def main():
     st.title("Stock analyzer")
-    company_name = st.text_input('Company name:', key='companyname')
+    company_name = st.text_input('Company name:', key=count)
 
     analyze_button = st.button("Analyze")
 
