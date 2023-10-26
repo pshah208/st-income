cc929d37f83719bc54813fed11f27cd84d7c8ba2
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-26 14:23:42 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -16,15 +16,14 @@ load_dotenv()
 if "openai.api_key" in st.secrets:
     openai.api_key = st.secrets.openai_api_key
 else:
-    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password", key='api_key_input_' + str(uuid.uuid4()))
+    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 if not openai.api_key:
-    st.title("Hello, I'm your Stock Analyzer")
+    st.title("Stock analyzer")
     st.info("Enter an OpenAI API Key to continue")
     st.info("If you are not sure on how to get your OpenAI API key:")
     st.info( " 1) Please visit https://platform.openai.com/account/api-keys")
     st.info(" 2) Click on 'Create new key' and copy and save the key in a safe location")
     st.stop()
-
 
 
 
