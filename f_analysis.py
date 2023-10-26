d58d520f6209401111fb7f2cfcc73bad86bab4e2
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-26 11:40:08 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -23,7 +23,7 @@ if not openai.api_key:
     st.info(" 2) Click on 'Create new key' and copy and save the key in a safe location")
     st.stop()
 st.title("Stock analyzer")
-company_name = st.text_input("Company name:")
+company_name = st.text_input("Company name:", key="company")
 
 #Define Functions
 def get_company_news(company_name):
