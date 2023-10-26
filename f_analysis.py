bc8038d5b0a51e682af6ef7ac71812360ff6259f
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-26 11:37:43 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -14,7 +14,7 @@ from langchain.tools import DuckDuckGoSearchResults
 if "openai.api_key" in st.secrets:
     openai.api_key = st.secrets.openai_api_key
 else:
-    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password", key="openai_key")
+    openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password")
 if not openai.api_key:
     st.title("Hello, I'm your stock analyser ")
     st.info("Enter an OpenAI API Key to continue")
@@ -22,6 +22,8 @@ if not openai.api_key:
     st.info( " 1) Please visit https://platform.openai.com/account/api-keys")
     st.info(" 2) Click on 'Create new key' and copy and save the key in a safe location")
     st.stop()
+st.title("Stock analyzer")
+company_name = st.text_input("Company name:")
 
 #Define Functions
 def get_company_news(company_name):
@@ -196,8 +198,7 @@ from f_analysis import financial_analyst
 
 
 def main():
-    st.title("Stock analyzer")
-    company_name = st.text_input("Company name:", key="company")
+    
     analyze_button = st.button("Analyze")
 
     if analyze_button:
