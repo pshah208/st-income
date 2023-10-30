8461e7af0e8c3427bec549d0bd5f31a713119114
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-30 16:37:07 -0400

    Update stanalysis.py

--- stanalysis.py
+++ stanalysis.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 import secrets
 from dotenv import find_dotenv, load_dotenv
 
-load_dotenv()
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
 serpapi = os.environ["SERPAPI_API_KEY"]
