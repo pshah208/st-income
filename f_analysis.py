ac69876f39b8875ccc5cb164e49426959669fee5
pshah208 <140012702+pshah208@users.noreply.github.com>
2023-10-30 13:21:58 -0400

    Update f_analysis.py

--- f_analysis.py
+++ f_analysis.py
@@ -10,15 +10,17 @@ import cons
 
 
 # Get an OpenAI API Key before continuing
-if "openai.api_key" in st.secrets:
- openai.api_key = st.secrets.openai_api_key
+if "openai_api_key" in st.secrets:
+    openai_api_key = st.secrets.openai_api_key
 else:
- openai.api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-   
-if not openai.api_key:
-      st.title("Stock analyzer")
-      st.info("Enter an OpenAI API Key to continue")
-      st.stop()
+    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+if not openai_api_key:
+    st.title("Hello, I'm Income Statement Analyzer 👓")
+    st.info("Enter an OpenAI API Key to continue")
+    st.info("If you are not sure on how to get your OpenAI API key:")
+    st.info( " 1) Please visit https://platform.openai.com/account/api-keys")
+    st.info(" 2) Click on 'Create new key' and copy and save the key in a safe location")
+    st.stop()
       
 os.environ["SERPAPI_API_KEY"] = cons.SERPAPI_API_KEY    
 
@@ -200,7 +202,7 @@ from f_analysis import financial_analyst
 def main():
     st.title("GenAI stock analyzer")
 
-    company_name = st.chat_input("Company name:")
+    company_name = st.text_input("Company name:")
     analyze_button = st.button("Analyze")
 
     if analyze_button:
